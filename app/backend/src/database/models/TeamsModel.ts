f6dc2ce3df0ca43639097e017ca75296ebd9530e
LuanaMayumi <luh.mayumi@gmail.com>
2023-06-13 18:28:20 -0300

    requisito 1

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,36 @@
+import { 
+  CreationOptional, 
+  InferAttributes, 
+  InferCreationAttributes,
+  DataTypes,
+  Model, 
+} from "sequelize";
+import db from '.';
+
+class TeamsModel extends Model<InferAttributes<TeamsModel>,
+InferCreationAttributes<TeamsModel>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+TeamsModel.init({
+  id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  teamName:{
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  }
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default TeamsModel;
+
