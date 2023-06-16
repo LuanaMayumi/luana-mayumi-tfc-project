4491c08b01ae2186dc551703596148708be8fdee
LuanaMayumi <luh.mayumi@gmail.com>
2023-06-16 12:11:54 -0300

    implementando corretamente a msg de retorno do req 8

--- app/backend/src/middlewares/ValidateLogin.ts
+++ app/backend/src/middlewares/ValidateLogin.ts
@@ -5,10 +5,11 @@ export default class ValidateLogin {
     const { email, password } = req.body;
     const emailRegex = /\S+@\S+.\S+/;
 
+    if (!email) { return res.status(400).json({ message: 'All fields must be filled' }); }
+    if (!password) { return res.status(400).json({ message: 'All fields must be filled' }); }
     if (!emailRegex.test(email)) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
-
     if (password.length <= 6) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
